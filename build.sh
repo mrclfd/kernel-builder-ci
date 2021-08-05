29369e7e34c1b70e794a2f7c2843de4edd77ec0f
dimas-ady <dimasady123@outlook.co.id>
2021-08-05 10:47:59 +0700

    Update

--- build.sh
+++ build.sh
@@ -228,13 +228,7 @@ build_kernel() {
 		make -j"$PROCS" O=out \
 		              CC=clang \
 		              CROSS_COMPILE=aarch64-linux-gnu- \ 
-		              CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-		              AR=llvm-ar \ 
-		              NM=llvm-nm \ 
-		              OBJCOPY=llvm-objcopy \ 
-		              OBJDUMP=llvm-objdump \ 
-		              STRIP=llvm-strip
-		              
+		              CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 	elif [ $COMPILER == clang ]
 	then
 	  make -j"$PROCS" O=out \
