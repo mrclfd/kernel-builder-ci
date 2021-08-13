04df194f71f5ebf099f3d9209644ff98ccb63d59
dimas-ady <dimasady123@outlook.co.id>
2021-08-13 17:10:39 +0700

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-KERNEL_REPO=https://github.com/dimas-ady/kernel_asus_sdm660 -b public
-
-git clone $KERNEL_REPO kernel
+KERNEL_REPO=https://github.com/dimas-ady/kernel_asus_sdm660
+BRANCH=public
+git clone $KERNEL_REPO -b $BRANCH kernel
 cd kernel
 bash ../build.sh
