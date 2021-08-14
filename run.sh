dcccbc128f58aeb6d531ec9c353b915129cc1ee5
dimas-ady <dimasady123@outlook.co.id>
2021-08-14 10:12:26 +0700

    Update

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-KERNEL_REPO=https://github.com/dimas-ady/kernel_asus_sdm660
+if [ -n "$DRONE" ]
+then
+  usermame=$DRONE_REPO_OWNER
+elif [ -n "$CIRCLECI" ]
+then
+  username=$CIRCLE_USERNAME
+fi
+
+KERNEL_REPO=https://$username:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660
 BRANCH=public
 git clone $KERNEL_REPO -b $BRANCH kernel
 cd kernel
