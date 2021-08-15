0ac2af1ca8c231f3ef482a0001ea363cfa6beaba
dimas-ady <dimasady123@outlook.co.id>
2021-08-15 07:28:47 +0700

    Update

--- run.sh
+++ run.sh
@@ -6,9 +6,8 @@ then
   username=$CIRCLE_USERNAME
 fi
 
-#KERNEL_REPO=https://$username:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660
-KERNEL_REPO=https://github.com/LineageOS/android_kernel_asus_sdm660.git
-BRANCH=lineage-18.1
+KERNEL_REPO=https://$username:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660
+BRANCH=public
 git clone $KERNEL_REPO -b $BRANCH kernel
 cd kernel
 bash ../build.sh
