bef0d7e32225f4dd7db9c8ebbdfa9881f0a267c8
dimas-ady <dimasady123@outlook.co.id>
2021-08-15 18:38:14 +0700

    Update

--- run.sh
+++ run.sh
@@ -8,6 +8,9 @@ fi
 
 KERNEL_REPO=https://$username:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660-p.git
 BRANCH=caf-test
+
+git config --global user.name $username
+git config --global user.email $GITHUB_EMAIL
 git clone $KERNEL_REPO -b $BRANCH kernel
 cd kernel
 bash ../build.sh
