2dd4627415a1a5e5fd1a3d5ddc5aeaa8c62f5d62
dimas-ady <dimasady123@outlook.co.id>
2021-08-15 18:48:45 +0700

    Compile Public Kernel

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ then
   username=$CIRCLE_USERNAME
 fi
 
-KERNEL_REPO=https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660-p.git
-BRANCH=caf-test
+KERNEL_REPO=https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660.git
+BRANCH=public
 
 git config --global user.name $GITHUB_USERNAME
 git config --global user.email $GITHUB_EMAIL
