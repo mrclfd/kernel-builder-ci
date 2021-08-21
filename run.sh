8592f095250c77dafa16a00d3f6aea642d3e9c56
dimas-ady <dimasady123@outlook.co.id>
2021-08-21 19:32:03 +0700

    Compile on another branch

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ then
 fi
 
 KERNEL_REPO=https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660-p.git
-BRANCH=lineage-18.1
+BRANCH=lineage-18.1-b
 
 git config --global user.name $GITHUB_USERNAME
 git config --global user.email $GITHUB_EMAIL
