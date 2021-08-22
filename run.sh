2c0cba18e3d3cf547a2c178eb7a1b0ea29b85b54
dimas-ady <dimasady123@outlook.co.id>
2021-08-22 09:12:22 +0700

    Update

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ then
 fi
 
 KERNEL_REPO=https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660-p.git
-BRANCH=lineage-18.1-b
+BRANCH=lineage-18.1
 
 git config --global user.name $GITHUB_USERNAME
 git config --global user.email $GITHUB_EMAIL
