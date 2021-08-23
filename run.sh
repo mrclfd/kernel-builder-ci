9fb46e57df87b0aa5eec1aa02b60535914c0f4ff
dimas-ady <dimasady123@outlook.co.id>
2021-08-24 06:41:22 +0700

    Compile CAF branch

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ then
 fi
 
 KERNEL_REPO=https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/dimas-ady/kernel_asus_sdm660-p.git
-BRANCH=lineage-18.1
+BRANCH=caf-test
 
 git config --global user.name $GITHUB_USERNAME
 git config --global user.email $GITHUB_EMAIL
